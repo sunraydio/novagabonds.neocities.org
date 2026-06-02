a65f09e0f869080d0c1cbfdd94f51e3c7ccc4240
Sun <rememberblueflowers@gmail.com>
2026-06-01 19:15:16 -0700

    tabination

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -44,3 +44,20 @@ function Demo_Cascading() {
           locationHash: false
         });
       }
+
+function showTab(index) {
+  // Retrieve tab content elements
+    let tabContents =
+        document.getElementsByClassName("tab-content");
+
+    // Loop through all tab content elements
+    for (let i = 0; i < tabContents.length; i++) {
+    
+        // Remove the "active" class from all tab content elements
+        tabContents[i].classList.remove("active");
+    }
+    
+    // Add the "active" class to the tab 
+    // content element at the specified index
+    tabContents[index].classList.add("active");
+}
