b7c8f25b341cf4c763b2083cb7fc3eba583c3fcf
Sun <rememberblueflowers@gmail.com>
2026-09-17 15:32:24 -0700

    s

---
+++ public/js/mousescroll.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 1) {
+      $('.arrow').addClass('fade');
+    } else{
+      $('.arrow').removeClass('fade');
+    }
+  })
+});
+
